5a6e67ca71264df3809c201f18c87db4fd77c539
al <al.n.popov@gmail.com>
2021-07-10 12:30:26 +0300

    stage_servers ADD

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,7 @@ resource "local_file" "HostsForAnsible" {
   content = templatefile("hosts.tpl",
     {
       build_servers = aws_instance.build_srv.*.public_ip
+      stage_servers = aws_instance.stage_srv.*.public_ip
     }
   )
   filename = "hosts"
